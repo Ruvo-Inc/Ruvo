89143cc0e519aec38fe09403df90adb7f18ac9ce
Pupinder Pathania <pathaniarajput92@gmail.com>
2024-05-06 16:54:00 +0530

    Wrapped google verfication in head tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,29 +4,30 @@ import Header from "@/app/layouts/Header/Header";
 import Aos from "@/app/components/UI/AOS/aos";
 import Footer from "./layouts/Footer/Footer";
 import { ThemeConfiger } from "./contexts/ThemeConfig/ThemeConfig";
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Ruvo - Home",
   description: "DRIVE CHANGE, RIDE GREEN",
-  verification: { google: "-U2s6Kf1Kj_0bM2QHU4x6NEypkj-a8YdPjOZGGLjbtU" }
+  verification: { google: "-U2s6Kf1Kj_0bM2QHU4x6NEypkj-a8YdPjOZGGLjbtU" },
 };
 
 export default function RootLayout({ children }) {
-
   return (
-    <html lang="en" >      
+    <html lang="en">
+      <head>
+        <GoogleAnalytics gaId="G-G49VLFBJRR" />
+      </head>
       <body className={inter.className}>
-      <GoogleAnalytics gaId="G-G49VLFBJRR" />
         <ThemeConfiger>
-        <Aos/>
-        <Header/>
-        {children}
-        <Footer/>
+          <Aos />
+          <Header />
+          {children}
+          <Footer />
         </ThemeConfiger>
-        </body>
+      </body>
     </html>
   );
 }
